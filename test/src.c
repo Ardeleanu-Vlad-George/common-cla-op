3f26c229afb427059f4ac6fb7c2f2629847896c6
Ardeleanu Vlad-George <ardeleanu_vlad@ymail.com>
2025-01-11 20:53:14 +0200

    Ran test, it worked

--- test/src.c
+++ test/src.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+#include "common_cla_op.h"
 
-int main(){
-  printf("Simple test file\n");
+int main(int argc, char* argv[]){
+  printf("Is the help command activated?\n%s\n", check_help_cmd(argc, argv) ? "YES" : "NO");
   return 0;
 }
